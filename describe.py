cc317153c7f1055ffcba40d0637d75100d93e56e
DaadfroGG <101118957+DaadfroGG@users.noreply.github.com>
2026-01-23 00:37:17 +0100

    actually cheating not on purpose sorry (todo reimplement describe)

--- describe.py
+++ describe.py
@@ -3,5 +3,5 @@ import pandas as pd
 from matplotlib import pyplot as plt
 
 df = pd.read_csv("datasets/dataset_train.csv")
-print(df.drop(columns = "Index").describe())
+print(df.drop(columns = "Index").describe()) #THATS CHEATING
 
--- histogram.py
+++ histogram.py
@@ -3,5 +3,5 @@ import pandas as pd
 from matplotlib import pyplot as plt
 
 df = pd.read_csv("datasets/dataset_train.csv")
-df.drop(columns = "Index").hist(alpha=0.5, bins=40)
+df.drop(columns = "Index").hist(alpha=0.5, bins=40) #THATS CHEATING ALSO APPARENTLY
 plt.show()
