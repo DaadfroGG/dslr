d2dc5cc7f67da158d07e9933eaba7072a911a942
DaadfroGG <d.kermia@yahoo.com>
2026-05-14 10:55:09 +0200

    removed maximisation

--- pair_plot.py
+++ pair_plot.py
@@ -3,7 +3,7 @@ import pandas as pd
 from matplotlib import pyplot as plt
 
 src = pd.read_csv("datasets/dataset_train.csv")
-fig, axes = plt.subplots(11,11)
+fig, axes = plt.subplots(13,13)
 ravenclaw = src.loc[src['Hogwarts House'] == 'Ravenclaw'].drop(columns = "Index")
 slytherin = src.loc[src['Hogwarts House'] == 'Slytherin'].drop(columns = "Index")
 gryffindor = src.loc[src['Hogwarts House'] == 'Gryffindor'].drop(columns = "Index")
@@ -18,13 +18,15 @@ for ax in axes:
 for house in range(len(houses)):
     df = houses[house]
     for i, col in enumerate(df.columns.values[6:]):
-        for j, row in enumerate(df.columns.values[6:6+i]):
+        for j, row in enumerate(df.columns.values[6:]):
             # df.plot.scatter(x=row, y=col, ax=axes[i-1][j], sharey=True, sharex=True, marker=",", s=1)
-            if i == 11:
-                df.plot.scatter(x=row, y=col, ax=axes[i-1][j],marker=".", s=1,alpha = 0.7, color = houses_color[house]).set(ylabel=None, yticklabels=[])            
-            elif j == 0:
-                df.plot.scatter(x=row, y=col, ax=axes[i-1][j],marker=".", s=1,alpha = 0.7, color = houses_color[house]).set(xlabel=None, xticklabels=[])
+            if j == i:
+                pass
+            # elif i == 13:
+            #     df.plot.scatter(x=row, y=col, ax=axes[i-1][j],marker=".", s=1,alpha = 0.7, color = houses_color[house]).set(ylabel=None, yticklabels=[])            
+            # elif j == 0:
+            #     df.plot.scatter(x=row, y=col, ax=axes[i-1][j],marker=".", s=1,alpha = 0.7, color = houses_color[house]).set(xlabel=None, xticklabels=[])
             else:
-                df.plot.scatter(x=row, y=col, ax=axes[i-1][j],marker=".", s=1,alpha = 0.7, color = houses_color[house]).set(xlabel=None,ylabel=None, xticklabels=[], yticklabels=[])
+                df.plot.scatter(x=row, y=col, ax=axes[i][j],marker=".", s=1,alpha = 0.7, color = houses_color[house]).set(xlabel=None,ylabel=None, xticklabels=[], yticklabels=[])
 
 plt.show()
